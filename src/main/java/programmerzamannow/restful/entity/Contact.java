7c7e34938238d3bec9d496e18a6641a754e959f3
Muhammad Reza Pahlevi <rezaworku31@gmail.com>
2026-05-05 16:43:22 +0700

    chore : Address entity initiated and Contact refactor list

--- src/main/java/programmerzamannow/restful/entity/Contact.java
+++ src/main/java/programmerzamannow/restful/entity/Contact.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Setter
 @Getter
 @AllArgsConstructor
@@ -30,4 +32,6 @@ public class Contact {
     @JoinColumn(name = "username", referencedColumnName = "username")
     private User user;
 
+    @OneToMany
+    private List<Address> addresses;
 }
