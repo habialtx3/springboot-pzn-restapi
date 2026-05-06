6fba756fae88cfee54407f177578c094fc88ce6d
Muhammad Reza Pahlevi <rezaworku31@gmail.com>
2026-05-06 15:46:35 +0700

    feat : duplicate post user api test

--- src/test/java/programmerzamannow/restful/controller/UserControllerTest.java
+++ src/test/java/programmerzamannow/restful/controller/UserControllerTest.java
@@ -7,9 +7,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import programmerzamannow.restful.entity.User;
 import programmerzamannow.restful.model.RegisterUserRequest;
 import programmerzamannow.restful.model.WebResponse;
 import programmerzamannow.restful.repository.UserRepository;
+import programmerzamannow.restful.security.Bcrypt;
 import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
 
@@ -80,5 +82,34 @@ class UserControllerTest {
             assertNotNull(response.getErrors());
         });
     }
+
+    @Test
+    void testRegisterDuplicate() throws Exception {
+        User user = new User();
+        user.setName("test");
+        user.setPassword(Bcrypt.hashpw("rahasia",Bcrypt.gensalt()));
+        user.setUsername("test");
+        userRepository.save(user);
+
+        RegisterUserRequest request = new RegisterUserRequest();
+        request.setUsername("test");
+        request.setPassword("rahasia");
+        request.setName("test");
+
+        mockMvc.perform(
+                post("/api/users")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+        ).andExpectAll(
+                status().isBadRequest()
+        ).andDo(result -> {
+            WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+            });
+
+            assertNotNull(response.getErrors());
+        });
+    }
+
 
 }
