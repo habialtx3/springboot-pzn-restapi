cb5e2d55b3492b510caf3df09dcd1a561f80528e
Muhammad Reza Pahlevi <rezaworku31@gmail.com>
2026-05-07 16:28:33 +0700

    refactor : contact controller, contact  service, changing contact mapped address, initialize create contact test bad request

---
+++ src/test/java/programmerzamannow/restful/controller/ContactControllerTest.java
@@ -0,0 +1,82 @@
+package programmerzamannow.restful.controller;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import programmerzamannow.restful.entity.User;
+import programmerzamannow.restful.model.ContactResponse;
+import programmerzamannow.restful.model.CreateContactRequest;
+import programmerzamannow.restful.model.WebResponse;
+import programmerzamannow.restful.repository.ContactRepository;
+import programmerzamannow.restful.repository.UserRepository;
+import programmerzamannow.restful.security.Bcrypt;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ContactControllerTest {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+
+        contactRepository.deleteAll();
+        userRepository.deleteAll();
+
+        User user = new User();
+        user.setUsername("test");
+        user.setName("test");
+        user.setPassword(Bcrypt.hashpw("passord", Bcrypt.gensalt()));
+        user.setToken("test");
+        user.setTokenExpiredAt(System.currentTimeMillis() + 1000000000);
+        userRepository.save(user);
+    }
+
+    @Test
+    void createContactBadRequest() throws Exception{
+        CreateContactRequest request = new CreateContactRequest();
+        request.setFirstName("");
+        request.setEmail("salah");
+
+        mockMvc.perform(
+                post("/api/contacts")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                        .header("X-API-TOKEN","test")
+        ).andExpectAll(
+                status().isBadRequest()
+        ).andDo(result -> {
+                    WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<WebResponse<String>>() {
+                    });
+                    assertNotNull(response.getErrors());
+                }
+        );
+    }
+
+
+}
